8116940136a43b7eab0c39c4dd991e26efeae87a
George Louthan <georgerlouth@nthefourth.com>
2017-06-06 23:14:13 -0500

    Multiplexing is now asynchronous with the writes of new data.

--- ccs_workspace/qc14_app/Startup/main.c
+++ ccs_workspace/qc14_app/Startup/main.c
@@ -4,6 +4,7 @@
 #include <ti/drivers/PIN.h>
 #include <ti/drivers/SPI.h>
 #include <ti/drivers/spi/SPICC26XXDMA.h>
+#include <ti/drivers/dma/UDMACC26XX.h>
 #include <ti/drivers/pin/PINCC26XX.h>
 #include <ti/drivers/timer/GPTimerCC26XX.h>
 #include <ti/drivers/PWM.h>
@@ -69,27 +70,90 @@ PIN_Config mbi_pin_table[] = {
     PIN_TERMINATE
 };
 
-#define MBI_CMD_VSYNC 2
-#define MBI_CMD_LATCH 1
-#define MBI_CMD_PREACT 14
-#define MBI_CMD_READCFG1 5
-#define MBI_CMD_CFG1 4
-#define MBI_CMD_CFG2 8
-#define MBI_CMD_RST 10
+SPI_Handle tlc_spi;
+
+#define TLC_THISISGS    0x00
+#define TLC_THISISFUN   0x01
+
+uint8_t fun_base[] = {
+        TLC_THISISFUN, // This is a command.
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // ...reserved...
+        // B135 / PSM(D1)       0
+        // B134 / PSM(D0)       0
+        // B133 / OLDENA        1
+        // B132 / IDMCUR(D1)    0
+        // B131 / IDMCUR(D0)    0
+        // B130 / IDMRPT(D0)    0
+        // B129 / IDMENA        0
+        // B128 / LATTMG(D1)    1:
+        0b00100001,
+        // B127 / LATTMG(D0)    1
+        // B126 / LSDVLT(D1)    0
+        // B125 / LSDVLT(D0)    0
+        // B124 / LODVLT(D1)    0
+        // B123 / LODVLT(D0)    0
+        // B122 / ESPWM         1
+        // B121 / TMGRST        1
+        // B120 / DSPRPT        1:
+        0b10000111,
+        // B119 / BLANK
+        // and 7 bits of global brightness correction:
+//        0xff, // blank on.
+        0x00, // after this there are 16 7-tets of dot correction. // blank off
+        //We'll send it as 14 octets.
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+};
+
+uint8_t rx_buf[33];
+
+uint8_t all_on[] = {
+        TLC_THISISGS, // This is a command.
+        0x00, 0x00,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+};
+
+uint8_t tlc_gs_buf[] = {
+        TLC_THISISGS, // This is a command.
+        0x00, 0x00,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+        0xff, 0xff,
+};
+
 volatile uint16_t scan_line = 0;
 volatile uint16_t gclk = 0;
-volatile uint8_t mbi_needs_vsync = 0;
-volatile uint8_t mbi_writing = 0;
-volatile uint8_t mbi_wr_sl = 0;
-volatile uint8_t mbi_wr_ch = 0;
-volatile uint8_t mbi_wr_bit = 0;
-volatile uint8_t mbi_gclk_hold = 0;
-volatile uint8_t mbi_vsync_ready = 0;
-volatile uint8_t mbi_vsync_wait = 0;
 Clock_Handle gclk_clock;
 Clock_Handle sw_clock;
 
-Semaphore_Handle led_load_sem;
+Semaphore_Handle led_tick_sem;
 
 #define LED_MP_CYCLES 64
 
@@ -108,23 +172,7 @@ void init_ble() {
 
 }
 
-
-uint16_t mbi_r_dat(uint16_t dat) {
-    uint16_t bit = 16;
-    uint16_t val = 0x0000;
-
-    while (bit) {
-        bit--;
-        PIN_setOutputValue(mbi_pin_h, LED_DOUT, (dat>>bit) & 0x0001);
-        val |= (PIN_getInputValue(LED_DIN) << bit);
-        PIN_setOutputValue(mbi_pin_h, LED_CLK, 1);
-        PIN_setOutputValue(mbi_pin_h, LED_CLK, 0);
-    }
-
-    return val;
-}
-
-void mp_shift() {
+inline void mp_shift() {
     scan_line++;
     scan_line %= 15;
     PIN_setOutputValue(mbi_pin_h, MP0_OUT, scan_line != 0);
@@ -173,7 +221,6 @@ void sw_clock_f() {
         // left clicked
         sw_c_clicked = 1;
         // do stuff
-        Semaphore_post(led_load_sem);
     } else if (sw_c_curr && sw_c_last && sw_c_clicked) {
         sw_c_clicked = 0;
         // unclicked.
@@ -184,10 +231,6 @@ void sw_clock_f() {
 volatile uint8_t gclk_state = 0;
 
 GPTimerCC26XX_Handle gclk_timer_h;
-
-void mp_tick() {
-    mp_shift();
-}
 
 void timer_init_task();
 
@@ -222,71 +265,70 @@ void init_switch() {
 
 }
 
-Task_Struct qcLoadTask;
-char qcLoadTaskStack[400];
+uint8_t led_buf[11][7][3] = {
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0,0,0}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0,0,0}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0,0,0}},
+    {{0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0xff, 0xff, 0xff}, {0,0,0}},
+};
 
-void qc_load_led_fn(UArg a0, UArg a1) {
-    while (1) {
-        Semaphore_pend(led_load_sem, BIOS_WAIT_FOREVER);
+
+uint8_t tlc_led_map[15][15][3] = {
+    { {0, 0, 0}, {0, 0, 1}, {0, 0, 2}, {1, 0, 0}, {1, 0, 1}, {1, 0, 2}, {2, 0, 0}, {2, 0, 1}, {2, 0, 2}, {3, 0, 0}, {3, 0, 1}, {3, 0, 2}, {4, 0, 0}, {4, 0, 1}, {4, 0, 2}},
+    { {0, 1, 0}, {0, 1, 1}, {0, 1, 2}, {1, 1, 0}, {1, 1, 1}, {1, 1, 2}, {2, 1, 0}, {2, 1, 1}, {2, 1, 2}, {3, 1, 0}, {3, 1, 1}, {3, 1, 2}, {4, 1, 0}, {4, 1, 1}, {4, 1, 2}},
+    { {0, 2, 0}, {0, 2, 1}, {0, 2, 2}, {1, 2, 0}, {1, 2, 1}, {1, 2, 2}, {2, 2, 0}, {2, 2, 1}, {2, 2, 2}, {3, 2, 0}, {3, 2, 1}, {3, 2, 2}, {4, 2, 0}, {4, 2, 1}, {4, 2, 2}},
+    { {0, 3, 0}, {0, 3, 1}, {0, 3, 2}, {1, 3, 0}, {1, 3, 1}, {1, 3, 2}, {2, 3, 0}, {2, 3, 1}, {2, 3, 2}, {3, 3, 0}, {3, 3, 1}, {3, 3, 2}, {4, 3, 0}, {4, 3, 1}, {4, 3, 2}},
+    { {0, 4, 0}, {0, 4, 1}, {0, 4, 2}, {1, 4, 0}, {1, 4, 1}, {1, 4, 2}, {2, 4, 0}, {2, 4, 1}, {2, 4, 2}, {3, 4, 0}, {3, 4, 1}, {3, 4, 2}, {4, 4, 0}, {4, 4, 1}, {4, 4, 2}},
+    { {0, 5, 0}, {0, 5, 1}, {0, 5, 2}, {1, 5, 0}, {1, 5, 1}, {1, 5, 2}, {2, 5, 0}, {2, 5, 1}, {2, 5, 2}, {3, 5, 0}, {3, 5, 1}, {3, 5, 2}, {4, 5, 0}, {4, 5, 1}, {4, 5, 2}},
+    { {0, 6, 0}, {0, 6, 1}, {0, 6, 2}, {1, 6, 0}, {1, 6, 1}, {1, 6, 2}, {2, 6, 0}, {2, 6, 1}, {2, 6, 2}, {3, 6, 0}, {3, 6, 1}, {3, 6, 2}, {4, 6, 0}, {4, 6, 1}, {4, 6, 2}},
+    { {5, 1, 0}, {5, 1, 1}, {5, 1, 2}, {5, 2, 0}, {5, 2, 1}, {5, 2, 2}, {5, 3, 0}, {5, 3, 1}, {5, 3, 2}, {5, 4, 0}, {5, 4, 1}, {5, 4, 2}, {5, 5, 0}, {5, 5, 1}, {5, 5, 2}},
+    { {6, 1, 0}, {6, 1, 1}, {6, 1, 2}, {6, 2, 0}, {6, 2, 1}, {6, 2, 2}, {6, 3, 0}, {6, 3, 1}, {6, 3, 2}, {6, 4, 0}, {6, 4, 1}, {6, 4, 2}, {6, 5, 0}, {6, 5, 1}, {6, 5, 2}},
+    { {5, 0, 0}, {5, 0, 1}, {5, 0, 2}, {6, 0, 0}, {6, 0, 1}, {6, 0, 2}, {5, 6, 0}, {5, 6, 1}, {5, 6, 2}, {6, 6, 0}, {6, 6, 1}, {6, 6, 2}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    { {7, 1, 0}, {7, 1, 1}, {7, 1, 2}, {7, 2, 0}, {7, 2, 1}, {7, 2, 2}, {7, 3, 0}, {7, 3, 1}, {7, 3, 2}, {7, 4, 0}, {7, 4, 1}, {7, 4, 2}, {7, 5, 0}, {7, 5, 1}, {7, 5, 2}},
+    { {10, 5, 0}, {10, 5, 1}, {10, 5, 2}, {10, 2, 0}, {10, 2, 1}, {10, 2, 2}, {10, 1, 0}, {10, 1, 1}, {10, 1, 2}, {10, 0, 0}, {10, 0, 1}, {10, 0, 2}, {10, 3, 0}, {10, 3, 1}, {10, 3, 2}},
+    { {9, 5, 0}, {9, 5, 1}, {9, 5, 2}, {9, 2, 0}, {9, 2, 1}, {9, 2, 2}, {9, 1, 0}, {9, 1, 1}, {9, 1, 2}, {9, 4, 0}, {9, 4, 1}, {9, 4, 2}, {9, 3, 0}, {9, 3, 1}, {9, 3, 2}},
+    { {8, 3, 0}, {8, 3, 1}, {8, 3, 2}, {8, 4, 0}, {8, 4, 1}, {8, 4, 2}, {8, 2, 0}, {8, 2, 1}, {8, 2, 2}, {8, 1, 0}, {8, 1, 1}, {8, 1, 2}, {8, 5, 0}, {8, 5, 1}, {8, 5, 2}},
+    { {7, 0, 0}, {7, 0, 1}, {7, 0, 2}, {10, 4, 0}, {10, 4, 1}, {10, 4, 2}, {9, 0, 0}, {9, 0, 1}, {9, 0, 2}, {8, 0, 0}, {8, 0, 1}, {8, 0, 2}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+};
+
+uint8_t screen2[3][3] = {{255, 255, 255,}, {255, 255, 255,}, {255, 255, 255,},};
+
+SPI_Transaction gs_data_transaction;
+
+void mp_tick() {
+    for (uint8_t i=0; i<15; i++) {
+        tlc_gs_buf[2 + i*2] = 0x00; //*tlc_led_map[scan_line][i];
+        tlc_gs_buf[2 + i*2 + 1] = 0xff; // MSB
+        //            tlc_gs_buf[2 + i*2 + 1] = led_buf[tlc_led_map[scan_line][i][0]][tlc_led_map[scan_line][i][1]][tlc_led_map[scan_line][i][2]];
     }
+
+    gs_data_transaction.txBuf = tlc_gs_buf;
+    gs_data_transaction.rxBuf = NULL;
+    gs_data_transaction.count = sizeof all_on;
+    SPI_transfer(tlc_spi, &gs_data_transaction);
 }
 
-#define TLC_THISISGS    0x00
-#define TLC_THISISFUN   0x01
+void tlc_spi_cb(SPI_Handle handle, SPI_Transaction *transaction) {
+    // We only have the one SPI, so we can pretty well ignore these parameters.
+    // TODO: We may want to check whether transaction.status == SPI_TRANSFER_COMPLETED???
+    // LATCH!
+    static uint16_t hwiKey;
+    // Disable hardware interrupts.
+    hwiKey  = (uint16_t) Hwi_disable();
 
-uint8_t fun_base[] = {
-        TLC_THISISFUN, // This is a command.
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // ...reserved...
-        // B135 / PSM(D1)       0
-        // B134 / PSM(D0)       0
-        // B133 / OLDENA        1
-        // B132 / IDMCUR(D1)    0
-        // B131 / IDMCUR(D0)    0
-        // B130 / IDMRPT(D0)    0
-        // B129 / IDMENA        0
-        // B128 / LATTMG(D1)    1:
-        0b00100001,
-        // B127 / LATTMG(D0)    1
-        // B126 / LSDVLT(D1)    0
-        // B125 / LSDVLT(D0)    0
-        // B124 / LODVLT(D1)    0
-        // B123 / LODVLT(D0)    0
-        // B122 / ESPWM         1
-        // B121 / TMGRST        1
-        // B120 / DSPRPT        1:
-        0b10000111,
-        // B119 / BLANK
-        // and 7 bits of global brightness correction:
-//        0xff, // blank on.
-        0x00,
-//        0x7f, // after this there are 16 7-tets of dot correction. // blank off
-        //We'll send it as 14 octets.
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-};
+    PIN_setOutputValue(mbi_pin_h, LED_LE, 1);
+    if (((uint8_t *)transaction->txBuf)[0] == TLC_THISISGS) mp_shift();
+    PIN_setOutputValue(mbi_pin_h, LED_LE, 0);
 
-uint8_t all_on[] = {
-        TLC_THISISGS, // This is a command.
-        0x00, 0x00,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-        0xff, 0xff,
-};
-
-uint8_t rx_buf[33];
+    Hwi_restore(hwiKey);
+}
 
 Task_Struct qcTask;
 char qcTaskStack[2000];
@@ -299,9 +341,8 @@ void qc_task_fn(UArg a0, UArg a1)
     gpt_params.width = GPT_CONFIG_16BIT;
     gclk_timer_h = GPTimerCC26XX_open(QC14BOARD_GPTIMER0A, &gpt_params);
 
-    GPTimerCC26XX_setLoadValue(gclk_timer_h, 24000);
+    GPTimerCC26XX_setLoadValue(gclk_timer_h, 25000);
     GPTimerCC26XX_registerInterrupt(gclk_timer_h, mp_tick, GPT_INT_TIMEOUT);
-    GPTimerCC26XX_start(gclk_timer_h);
 
     PWM_Handle gclk_pwm_h;
     PWM_Params pwm_params;
@@ -311,7 +352,7 @@ void qc_task_fn(UArg a0, UArg a1)
     PWM_Params_init(&pwm_params);
     pwm_params.idleLevel = PWM_IDLE_LOW;
     pwm_params.periodUnits = PWM_PERIOD_HZ;
-    pwm_params.periodValue = 10000000;
+    pwm_params.periodValue = 24000000;
     pwm_params.dutyUnits = PWM_DUTY_FRACTION;
     pwm_params.dutyValue = PWM_DUTY_FRACTION_MAX/2;
 
@@ -323,15 +364,6 @@ void qc_task_fn(UArg a0, UArg a1)
 
     Error_Block eb;
 
-    led_load_sem = Semaphore_create(0, NULL, &eb);
-
-    Task_Params taskParams;
-    Task_Params_init(&taskParams);
-    taskParams.stack = qcLoadTaskStack;
-    taskParams.stackSize = sizeof(qcLoadTaskStack);
-    taskParams.priority = 1;
-    Task_construct(&qcLoadTask, qc_load_led_fn, &taskParams, NULL);
-
     ADC_Handle adc;
     ADC_Params adcp;
     ADC_init();
@@ -339,40 +371,25 @@ void qc_task_fn(UArg a0, UArg a1)
     ADC_Params_init(&adcp);
     adc = ADC_open(QC14BOARD_ADC7_LIGHT, &adcp);
 
-    SPI_Handle tlc_spi;
     SPI_Params tlc_spi_params;
 
     SPI_Params_init(&tlc_spi_params);
-    // Defaults should be good.
-
-    tlc_spi_params.mode = SPI_MODE_BLOCKING;
-    tlc_spi_params.bitRate = 100000;
-
+    tlc_spi_params.transferMode = SPI_MODE_CALLBACK;
+    tlc_spi_params.mode = SPI_MASTER;
+    tlc_spi_params.transferCallbackFxn = tlc_spi_cb;
+    tlc_spi_params.bitRate = 6000000;
     tlc_spi = SPI_open(QC14BOARD_TLC_SPI, &tlc_spi_params);
 
     SPI_Transaction fun_data_transaction;
-
     fun_data_transaction.txBuf = fun_base;
     fun_data_transaction.rxBuf = NULL;
     fun_data_transaction.count = sizeof fun_base;
 
     SPI_transfer(tlc_spi, &fun_data_transaction);
-    //     LATCH!
-    PIN_setOutputValue(mbi_pin_h, LED_LE, 1);
-    Task_sleep(1);
-    PIN_setOutputValue(mbi_pin_h, LED_LE, 0);
 
+    while (fun_data_transaction.status != SPI_TRANSFER_COMPLETED);
 
-    SPI_Transaction gs_data_transaction;
-    gs_data_transaction.txBuf = all_on;
-    gs_data_transaction.rxBuf = rx_buf;
-    gs_data_transaction.count = sizeof all_on;
-    SPI_transfer(tlc_spi, &gs_data_transaction);
-    // LATCH!
-    PIN_setOutputValue(mbi_pin_h, LED_LE, 1);
-    Task_sleep(1);
-    PIN_setOutputValue(mbi_pin_h, LED_LE, 0);
-
+    GPTimerCC26XX_start(gclk_timer_h);
 
     UART_init();
 
@@ -396,11 +413,11 @@ void qc_task_fn(UArg a0, UArg a1)
         Task_sleep(5000);
     } while (1);
 
-
-    while (1)
-    {
-        Task_sleep(BIOS_WAIT_FOREVER);
-    }
+//
+//    while (1)
+//    {
+//        Task_sleep(BIOS_WAIT_FOREVER);
+//    }
 }
 
 int main()
@@ -408,7 +425,7 @@ int main()
 
   PIN_init(BoardGpioInitTable);
 
-//  Power_setDependency(XOSC_HF); // TODO
+  Power_setDependency(PowerCC26XX_XOSC_HF);
 
 #ifndef POWER_SAVING
   /* Set constraints for Standby, powerdown and idle mode */
--- etc/led_transform_generator.py
+++ etc/led_transform_generator.py
@@ -1,10 +1,6 @@
 import pprint
 
 arms = [
-    [71, 51, 52, 53, 54, 55],
-    [74, 69, 68, 66, 67, 70],
-    [73, 63, 62, 65, 64, 61],
-    [59, 58, 57, 60, 72, 56]
 ]
 
 screen = [
@@ -15,6 +11,10 @@ screen = [
     [5, 10, 15, 20, 25, 30, 35],
     [46, 36, 37, 38, 39, 40, 48],
     [47, 41, 42, 43, 44, 45, 49],
+    [71, 51, 52, 53, 54, 55],
+    [74, 69, 68, 66, 67, 70],
+    [73, 63, 62, 65, 64, 61],
+    [59, 58, 57, 60, 72, 56]
 ]
 
 screen_transforms = []
@@ -22,28 +22,29 @@ screen_transforms = []
 for i in range(15):
     screen_transforms.append(['']*15)
         
-for arm_index in range(4):
-    for led_index in range(6):
-        led_id = arms[arm_index][led_index]
-        channel = ((led_id-1) % 5) * 3
-        scanline = int((led_id-1)/5)
-        print scanline, channel
-        for color in range(3):
-            screen_transforms[scanline][channel+color] = '&arms[%d][%d][%d]' % (arm_index, led_index, color)
+# for arm_index in range(4):
+    # for led_index in range(6):
+        # led_id = arms[arm_index][led_index]
+        # channel = ((led_id-1) % 5) * 3
+        # scanline = int((led_id-1)/5)
+        # print scanline, channel
+        # for color in range(3):
+            # screen_transforms[scanline][channel+color] = '&(arms[%d][%d][%d])' % (arm_index, led_index, color)
 
-for screen_row in range(7):
-    for screen_column in range(7):
+for screen_row in range(11):
+    for screen_column in range(len(screen[screen_row])):
         led_id = screen[screen_row][screen_column]
         channel = ((led_id-1) % 5) * 3
         scanline = int((led_id-1)/5)
         print scanline, channel
         for color in range(3):
-            screen_transforms[scanline][channel+color] = '&screen[%d][%d][%d]' % (screen_column, screen_row, color)
+            screen_transforms[scanline][channel+color] = '{%d, %d, %d}' % (screen_row, screen_column, color)
+            #screen_transforms[scanline][channel+color] = '&(screen[%d][%d][%d])' % (screen_row, screen_column, color)
             
 for scanline in range(15):
     for channel in range(15):
         if not screen_transforms[scanline][channel]:
-            screen_transforms[scanline][channel] = '&dontcare'
+            screen_transforms[scanline][channel] = '{0, 0, 0}'
             
 pprint.pprint(screen_transforms)
 
