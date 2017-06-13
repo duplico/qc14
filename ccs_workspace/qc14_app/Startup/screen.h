ba8a18001082f560ff252b215b30d8ea4616c7e3
George Louthan <191906+duplico@users.noreply.github.com>
2017-06-13 00:49:04 -0500

    Tweak colors and add screen animation demo.

---
+++ ccs_workspace/qc14_app/Startup/screen.h
@@ -0,0 +1,13 @@
+/*
+ * screen.h
+ *
+ *  Created on: Jun 12, 2017
+ *      Author: George
+ */
+
+#ifndef STARTUP_SCREEN_H_
+#define STARTUP_SCREEN_H_
+
+void screen_init();
+
+#endif /* STARTUP_SCREEN_H_ */
