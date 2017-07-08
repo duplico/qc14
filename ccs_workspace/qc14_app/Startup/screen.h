ce8ff77006d07f3e23f587e9c58f44eb0ed2b03e
George Louthan <191906+duplico@users.noreply.github.com>
2017-07-08 15:54:05 -0500

    Create some frame structs (#28) and workaround BLE issue (resolve #19).

--- ccs_workspace/qc14_app/Startup/screen.h
+++ ccs_workspace/qc14_app/Startup/screen.h
@@ -8,6 +8,21 @@
 #ifndef STARTUP_SCREEN_H_
 #define STARTUP_SCREEN_H_
 
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} rgbcolor_t;
+
+typedef struct {
+    rgbcolor_t pixels[7][7];
+    uint8_t pad;
+} screen_frame_t;
+
+typedef struct {
+    rgbcolor_t pixels[6];
+} arm_frame_t;
+
 void screen_init();
 void screen_blink_on();
 void screen_blink_off();
