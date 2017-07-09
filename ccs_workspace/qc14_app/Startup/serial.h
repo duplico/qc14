bc785999910915672b945a2d9f902e3c67ff935f
George Louthan <georgerlouth@nthefourth.com>
2017-07-08 21:56:33 -0500

    Crashy implementation of the serial comms state machine.

--- ccs_workspace/qc14_app/Startup/serial.h
+++ ccs_workspace/qc14_app/Startup/serial.h
@@ -8,6 +8,49 @@
 #ifndef STARTUP_SERIAL_H_
 #define STARTUP_SERIAL_H_
 
+#define PROTO_STATE_DIS 0
+#define PROTO_STATE_PLUGGING 1
+#define PROTO_STATE_IDLE 2
+#define PROTO_STATE_RTS_WAIT 3
+#define PROTO_STATE_CTS_WAIT 4
+#define PROTO_STATE_RTS_OUT 5
+
+#define SERIAL_MSG_TYPE_NOMSG 0
+#define SERIAL_MSG_TYPE_HANDSHAKE 1
+#define SERIAL_MSG_TYPE_GAME 2
+#define SERIAL_MSG_TYPE_TILE 3
+#define SERIAL_MSG_TYPE_CONF 4
+
 void serial_init();
+
+typedef struct {
+    uint16_t badge_id;
+    uint8_t msg_type;
+    uint64_t current_time;
+    uint16_t current_time_authority;
+    uint8_t payload[50];
+    uint16_t crc;
+} serial_message_t;
+
+typedef struct {
+    char handle[9];
+    uint8_t badges_mated[36];
+    uint8_t current_mode;
+    uint8_t current_icon_or_tile_id;
+    uint8_t pad[3];
+} serial_handshake_t; // Initialization handshake
+
+typedef struct {
+
+} serial_game_msg; // Game update
+
+typedef struct {
+
+} serial_tile_msg; // Tile topology/info
+
+typedef struct {
+    uint8_t update_handle;
+    char new_handle[9];
+} serial_conf_msg; // INBOUND FROM CONSOLE
 
 #endif /* STARTUP_SERIAL_H_ */
