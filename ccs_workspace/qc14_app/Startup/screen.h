e53863d5d51e9f2f670af8569d198ac0ca380a9b
George Louthan <191906+duplico@users.noreply.github.com>
2017-07-11 20:10:38 -0500

    Successful gif animation test.

--- ccs_workspace/qc14_app/Startup/screen.h
+++ ccs_workspace/qc14_app/Startup/screen.h
@@ -23,6 +23,26 @@ typedef struct {
     rgbcolor_t pixels[6];
 } arm_frame_t;
 
+typedef struct {
+    uint32_t anim_start_frame;
+    uint16_t anim_len;
+    uint16_t anim_frame_delay_ms;
+} screen_anim_t;
+
+typedef struct {
+    uint16_t mate_icon_id;
+    uint16_t result_icon_id;
+    uint16_t arm_anim_id;
+    uint8_t sufficient_flag;
+    uint8_t other_arm_id;
+} mate_spec_t;
+
+typedef struct {
+    uint16_t id;
+    screen_anim_t animation;
+    mate_spec_t arms[4];
+} game_icon_t;
+
 void screen_init();
 void screen_blink_on();
 void screen_blink_off();
